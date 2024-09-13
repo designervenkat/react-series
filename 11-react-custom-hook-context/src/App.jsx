426d211a4dce4f8661426eef42fe42cb8d1b96eb
arcs_mr <murty.av@gmail.com>
2024-09-13 18:30:48 +0530

    11-react-custom-hook-context-initial-setup

---
+++ 11-react-custom-hook-context/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h2 className="text-2xl">React Custom Hook and Context API</h2>
+        </>
+    )
+}
+
+export default App
