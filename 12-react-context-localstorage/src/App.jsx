9c58a920cb0a50a6552b15ac270f6315769e4ed5
arcs_mr <murty.av@gmail.com>
2024-09-16 20:09:33 +0530

    Initial commit

---
+++ 12-react-context-localstorage/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <p className="read-the-docs">
+                Todo APP with context api and local storage
+            </p>
+        </>
+    )
+}
+
+export default App
